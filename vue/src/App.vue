4332f93a550351fe6db861b13d8ab5d14d983769
Artus <artuslama@gmail.com>
2022-12-01 19:50:54 +0100

    initial

---
+++ vue/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>WELCOME</h1>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+  @import 'css/style.css';
+</style>
