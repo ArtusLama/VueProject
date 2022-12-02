b5d4fc59c0843b2394fb770d31673343b38fc349
Artus <artuslama@gmail.com>
2022-12-02 13:52:18 +0100

    add backend

---
+++ backend/scripts/hello_world.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+
+
+const app = express();
+const port = 3000;
+
+
+app.listen(port, () => console.log(`Hello world app listening on port ${port}!`))
+
+app.get("/", (req, res) => {
+    res.send("");
+});
+
+app.get("/ping", (req, res) => {
+    res.send("Hello World, from express");
+});
