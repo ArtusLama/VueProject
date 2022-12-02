2a21b65158893596200f19e8541166d71c8130cc
Artus <artuslama@gmail.com>
2022-12-02 14:21:27 +0100

    add routes js

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,40 @@
-const express = require('express');
+
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require('routes/routes');
 
 
 
+// DATABASE
+const databaseLink = process.env.DATABASE;
+
+mongoose.connect(databaseLink);
+const database = mongoose.connection;
+
+
+database.on("error", (error) => {
+    console.log("[DATABASE] -> " + error)
+})
+database.once("connected", () => {
+    console.log("[DATABASE] -> Connected to database!");
+})
+
+
+
+
+
+
+// BACKEND
 const app = express();
 const port = 3000;
 
+app.listen(port, () => console.log(`Server started at port: ${port}!`))
+app.use('/api', routes)
 
-app.listen(port, () => console.log(`Hello world app listening on port ${port}!`))
 
+// BACKEND SITES
 app.get("/", (req, res) => {
     res.send("Welcome to the Backend!");
 });
